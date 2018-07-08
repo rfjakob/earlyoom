607b673c355311606e41ed8eb86d9d7b882d7be4
Jakob Unterwurzacher <jakobunt@gmail.com>
2018-07-08 12:37:48 +0200

    tests: verifiy that "-r 0" works
    This should disable the periodic memory reports.

--- tests/cli_test.go
+++ tests/cli_test.go
@@ -16,14 +16,19 @@ type cliTestCase struct {
 
 func TestCli(t *testing.T) {
 	// The periodic memory report looks like this:
-	// mem avail: 4998 MiB (63 %), swap free: 0 MiB (0 %)
+	//   mem avail: 4998 MiB (63 %), swap free: 0 MiB (0 %)
 	const memReport = "mem avail: "
-
+	// earlyoom startup looks like this:
+	//   earlyoom v1.1-5-g74a364b-dirty
+	//   mem total: 7836 MiB, min: 783 MiB (10 %)
+	//   swap total: 0 MiB, min: 0 MiB (10 %)
+	// startupMsg matches the last line of the startup output.
+	const startupMsg = "swap total: "
 	testcases := []cliTestCase{
 		{args: []string{"-h"}, code: 0, stderrContains: "this help text", stdoutEmpty: true},
 		{args: []string{"-h"}, code: 0, stderrContains: "this help text", stdoutEmpty: true},
-		{args: nil, code: -1, stderrContains: "swap total", stdoutContains: memReport},
-		{args: []string{"-p"}, code: -1, stdoutContains: "mem avail: "},
+		{args: nil, code: -1, stderrContains: startupMsg, stdoutContains: memReport},
+		{args: []string{"-p"}, code: -1, stdoutContains: memReport},
 		{args: []string{"-v"}, code: 0, stderrContains: "earlyoom v", stdoutEmpty: true},
 		{args: []string{"-d"}, code: -1, stdoutContains: "^ new victim (higher badness)"},
 		{args: []string{"-m", "1"}, code: -1, stderrContains: "(1 %)", stdoutContains: memReport},
@@ -32,6 +37,7 @@ func TestCli(t *testing.T) {
 		{args: []string{"-s", "0"}, code: 16, stderrContains: "Invalid percentage", stdoutEmpty: true},
 		{args: []string{"-M", "1024"}, code: -1, stderrContains: "min: 1 MiB", stdoutContains: memReport},
 		{args: []string{"-S", "2048"}, code: -1, stderrContains: "min: 2 MiB", stdoutContains: memReport},
+		{args: []string{"-r", "0"}, code: -1, stderrContains: startupMsg, stdoutEmpty: true},
 	}
 
 	for i, tc := range testcases {
