41cb5f38184b34063d01d4aa96495e749efe68e3
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-04-27 13:49:09 +0200

    contrib: add zombie
    Simple toy program that creates a zombie process.

---
+++ contrib/zombie.c
@@ -0,0 +1,34 @@
+/*
+Creates a zombie child process.
+Automatically exits after 10 minutes.
+
+Should look like this in ps:
+
+$ ps auxwwww | grep zombie
+jakob     7513  0.0  0.0   2172   820 pts/1    S+   13:44   0:00 ./zombie
+jakob     7514  0.0  0.0      0     0 pts/1    Z+   13:44   0:00 [zombie] <defunct>
+*/
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main () {
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("fork failed");
+		exit(1);
+	}
+	if (pid > 0) {
+		/* parent */
+		sleep(600);
+		int wstatus;
+		wait(&wstatus);
+		exit(0);
+	} else {
+		/* child */
+		exit(0);
+	}
+}
