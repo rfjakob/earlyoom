017b9ddc9cb41b72dc296939966dacb9cf5f7424
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-06-01 21:35:13 +0200

    add contrib/fillmem.py

---
+++ contrib/fillmem.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# fillmem.py allocates a configurable amount of memory
+# and sleeps forever.
+#
+# Originally posted by swiftcoder at
+# https://unix.stackexchange.com/a/99365/45823
+
+import sys
+import time
+import os
+
+if len(sys.argv) != 2:
+    print("usage: ./fillmem.py MEGABYTES")
+    sys.exit()
+
+count = int(sys.argv[1])
+data = bytearray(1024*1024*count)
+os.system("grep VmRSS /proc/%d/status" % (os.getpid()))
+time.sleep(3600*999999)
