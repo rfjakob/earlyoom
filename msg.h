15f13f06a2e1c193b5ea2537f1044f3ce54d0156
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-03-01 21:07:04 +0100

    msg: add __attribute__((format))
    This was caught by https://lgtm.com/ , but should have
    already been caught by gcc. Enable the check.
    
    https://github.com/rfjakob/earlyoom/issues/163

--- msg.h
+++ msg.h
@@ -6,9 +6,13 @@
 
 #define MSG_LEN 256
 
-int fatal(int code, char* fmt, ...);
-int warn(const char* fmt, ...);
-int debug(const char* fmt, ...);
+/* From https://gcc.gnu.org/onlinedocs/gcc-9.2.0/gcc/Common-Function-Attributes.html :
+ * The format attribute specifies that a function takes printf
+ * style arguments that should be type-checked against a format string.
+ */
+int fatal(int code, char* fmt, ...) __attribute__((format(printf, 2, 3)));
+int warn(const char* fmt, ...) __attribute__((format(printf, 1, 2)));
+int debug(const char* fmt, ...) __attribute__((format(printf, 1, 2)));
 
 typedef struct {
     // If the conversion failed, err contains the error message.
