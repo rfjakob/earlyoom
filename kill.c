b97ea0c89e902ae58aa6eb56dae97058cf86ef90
huangran.nick <huangran.nick@bytedance.com>
2023-09-13 14:01:55 +0800

    add env EARLYOOM_CMDLINE in notify_ext()

--- kill.c
+++ kill.c
@@ -89,6 +89,7 @@ static void notify_ext(const char* script, const procinfo_t* victim)
     setenv("EARLYOOM_PID", pid_str, 1);
     setenv("EARLYOOM_UID", uid_str, 1);
     setenv("EARLYOOM_NAME", victim->name, 1);
+    setenv("EARLYOOM_CMDLINE", victim->cmdline, 1);
 
     execl(script, script, NULL);
     warn("%s: exec %s failed: %s\n", __func__, script, strerror(errno));
