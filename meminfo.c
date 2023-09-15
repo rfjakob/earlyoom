f07761c468603d922401b6cb844b49668b64ebe8
huangran.nick <huangran.nick@bytedance.com>
2023-09-15 10:43:22 +0800

    add env EARLYOOM_CMDLINE to MANPAGE.md

--- meminfo.c
+++ meminfo.c
@@ -251,7 +251,7 @@ int get_comm(int pid, char* out, size_t outlen)
 }
 
 
-/* Read /proc/[pid]/cmdline (process command line truncated to 1000 bytes).
+/* Read /proc/[pid]/cmdline (process command line truncated to 256 bytes).
  * Returns 0 on success and -errno on error.
  */
 int get_cmdline(int pid, char* out, size_t outlen)
